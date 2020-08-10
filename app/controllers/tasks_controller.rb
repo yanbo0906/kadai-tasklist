b33036d5b12b369d4456ae6ebefff9a600d10286
seiya.asano <yanbo0906@gmail.com>
2020-08-10 09:37:55 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,6 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
 private
 
@@ -54,3 +53,4 @@ private
   def task_params
     params.require(:task).permit(:content)
   end
+end
