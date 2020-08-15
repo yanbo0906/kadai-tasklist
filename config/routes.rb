151c1d1f340e6ef5cedecad9760887b145dd22e1
seiya.asano <yanbo0906@gmail.com>
2020-08-15 11:06:22 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
     root to:'tasks#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
     resources :tasks
-    resources :users, except: [:show, :index]
+    resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
