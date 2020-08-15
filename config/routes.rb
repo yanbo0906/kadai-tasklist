b8a46fbee906774c09be5ff733e8e82c77474ad5
seiya.asano <yanbo0906@gmail.com>
2020-08-15 10:42:49 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     
     get 'signup', to: 'users#new'
     resources :tasks
+    resources :users, except: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200809064137_create_tasks.rb
+++ db/migrate/20200809064137_create_tasks.rb
@@ -2,6 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :content
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
