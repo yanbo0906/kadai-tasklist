990125e3f87a002f60fd7079d5cc239bc8a45139
seiya.asano <yanbo0906@gmail.com>
2020-08-15 07:54:37 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root to:'tasks#index'
     
+    get 'signup', to: 'users#new'
     resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
