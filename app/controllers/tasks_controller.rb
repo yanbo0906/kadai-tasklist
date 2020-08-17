5a4bad4360d05257793a7cc5fee0c670b6c9b08d
seiya.asano <yanbo0906@gmail.com>
2020-08-17 11:29:25 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def index
-      @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
