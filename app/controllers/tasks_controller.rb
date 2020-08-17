532c982aaf6ee2485236c276d660391dea0e5552
seiya.asano <yanbo0906@gmail.com>
2020-08-17 13:20:22 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [ :show, :create ]
+  before_action :correct_user, only: [ :id]
   
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
