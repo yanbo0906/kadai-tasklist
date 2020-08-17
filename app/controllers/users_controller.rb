2a9ef4b49626bea2d70064a08f98af973d5629e5
seiya.asano <yanbo0906@gmail.com>
2020-08-17 09:45:35 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tasks = @user.order(id: :desc).page(params[:page])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
     counts(@user)
   end
 
